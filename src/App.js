5e8c674998d35e176e8a4d9d70797205f6b9db1f
chainsensedhiraj <dhiraj.patil@chainsense.in>
2022-06-29 16:17:47 +0530

    minor fix

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import logo from './logo.svg';
 import HomePage from './pages/Homepage';
 
 function App() {
-  const checkWalletIsConnected = () => {
-    if (!ethereum) {
-      alert('Please connect to your wallet');
-      return;
-    } else {
-      ethereum.enable();
+  const web3 = new Web3(Web3.givenProvider || 'https://data-seed-prebsc-1-s1.binance.org:8545');
+  web3.eth.getAccounts().then(res=>{
+    console.log(res);
+    console.log(web3.currentProvider);
+  });
+  // check web3 provider url
+  
 
-    }
-  }
   return (
 
     <div className="App">
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 
 export class HomePage extends Component {
   
-  const []
   render() {
     return (
       <div>HomePage</div>
